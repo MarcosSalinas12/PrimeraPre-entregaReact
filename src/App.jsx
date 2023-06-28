dfe9131260e8f37a12863e0cc434d72dad997587
Marcos Salinas <marcossalinas12nov@gmail.com>
2023-06-28 11:01:38 -0300

    Marcodp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/header/navigation-item'
+
+
+function App() {
+
+
+  return (
+<div>
+  <Header logo='MDp'/>
+</div>
+  )
+}
+
+export default App
